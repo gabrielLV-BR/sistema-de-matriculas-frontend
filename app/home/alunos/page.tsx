1bf0c72b2253d9127a69677593ebb6b672f42c99
Gabriel L <gabriellovato15@gmail.com>
2023-09-21 21:59:15 -0300

    Icons and page for Student listing

--- app/home/alunos/page.tsx
+++ app/home/alunos/page.tsx
@@ -1,3 +1,39 @@
+import Image from "next/image";
+import styles from "./styles.module.scss"
+
+type StudentItemProps = {
+    name: string;
+}
+
+function StudentItem(props: StudentItemProps) {
+    return (
+        <li className={styles['student-item']}>
+            <Image src="/icons/profile.svg" alt="Perfil do aluno" width={16} height={16} draggable={false} />
+            <span>
+                {props.name}
+            </span>
+
+            <Image src="/icons/edit.svg" alt="Editar" width={16} height={16} className={styles.edit} draggable={false} />
+        </li>
+    )
+}
+
 export default function StudentPage() {
-    return <p>Página dos alunuos</p>
+    const students = Array(
+        'Fulano',
+        'Sicrano',
+        'Beltrano'
+    )
+
+    return (
+        <div className={styles.container}>
+            <h1>Listagem de alunos</h1>
+            <hr />
+            <ul className={styles.listing}>
+                {
+                    students.map(x => StudentItem({ name: x }))
+                }
+            </ul>
+        </div>
+    )
 }
